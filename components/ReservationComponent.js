a99f1ddc14bc8e6dac4ae602f499527d0b6168eb
FrostyDesigner <drafter760@gmail.com>
2020-02-24 19:41:26 -0800

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, 
-    Picker, Switch, Button, Modal, Alert } from 'react-native';
+import {
+    Text, View, ScrollView, StyleSheet,
+    Picker, Switch, Button, Modal, Alert
+} from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+// for local notifications
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
 
     constructor(props) {
         super(props);
 
-    this.state = {
-        campers: 1,
-        hikeIn: false,
-        date: '',
-        showModal: false
-    };
-}
+        this.state = {
+            campers: 1,
+            hikeIn: false,
+            date: '',
+            showModal: false
+        };
+    }
 
     static navigationOptions = {
         title: 'Reserve Campsite'
@@ -28,27 +33,30 @@ class Reservation extends Component {
     handleReservation() {
         //Alert Popup message
         Alert.alert(
-        'Begin Search?',
-        ` Number of Campers: ${this.state.campers} 
+            'Begin Search?',
+            ` Number of Campers: ${this.state.campers} 
         \n Hike-In? ${this.state.hikeIn ? 'true' : 'false'} 
         \n Date: ${this.state.date}`,
 
-        //Show OK and Cancel to reset
-        [
-            { 
-            text: 'Cancel', 
-            onPress: () => this.resetForm(),
-            style: ' cancel'
-            },
-        {
-            text: 'OK',
-            onPress: () => this.resetForm()
-            }
-        ],
-        { cancelable: false }
-        );   
+            //Show OK and Cancel to reset
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => this.resetForm(),
+                    style: ' cancel'
+                },
+                {
+                    text: 'OK',
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();
+                    }
+                }
+            ],
+            { cancelable: false }
+        );
     }
-        //End 
+    //End 
 
     resetForm() {
         this.setState({
@@ -57,6 +65,28 @@ class Reservation extends Component {
             date: '',
             showModal: false
         });
+    }
+
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
     }
 
     // In ReservationComponent.js, replace the top-level ScrollView component in the
@@ -71,7 +101,7 @@ class Reservation extends Component {
                     <Picker
                         style={styles.formItem}
                         selectedValue={this.state.campers}
-                        onValueChange={itemValue => this.setState({campers: itemValue})}>
+                        onValueChange={itemValue => this.setState({ campers: itemValue })}>
                         <Picker.Item label='1' value='1' />
                         <Picker.Item label='2' value='2' />
                         <Picker.Item label='3' value='3' />
@@ -85,14 +115,14 @@ class Reservation extends Component {
                     <Switch
                         style={styles.formItem}
                         value={this.state.hikeIn}
-                        trackColor={{true: '#5637DD', false: null}}
-                        onValueChange={value => this.setState({hikeIn: value})}>
+                        trackColor={{ true: '#5637DD', false: null }}
+                        onValueChange={value => this.setState({ hikeIn: value })}>
                     </Switch>
                 </View>
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Date</Text>
                     <DatePicker
-                        style={{flex: 2, marginRight: 20}}
+                        style={{ flex: 2, marginRight: 20 }}
                         date={this.state.date}
                         format='YYYY-MM-DD'
                         mode='date'
@@ -111,7 +141,7 @@ class Reservation extends Component {
                                 marginLeft: 36
                             }
                         }}
-                        onDateChange={date => {this.setState({date: date})}}
+                        onDateChange={date => { this.setState({ date: date }) }}
                     />
                 </View>
                 <View style={styles.formRow}>
@@ -142,7 +172,7 @@ const styles = StyleSheet.create({
     formItem: {
         flex: 1
     },
-    modal: { 
+    modal: {
         justifyContent: 'center',
         margin: 20
     },
